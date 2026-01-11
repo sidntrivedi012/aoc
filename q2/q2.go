1334c9ba159644a18ccd64135f5bfa3bc616a12c
Siddhant Trivedi <strivedi+pure@purestorage.com>
2026-01-11 16:16:35 +0530

    q2(part2): kmp

--- q2/q2.go
+++ q2/q2.go
@@ -17,19 +17,39 @@ func Solve(lines []string) int {
 		endIndex, _ := strconv.Atoi(end)
 
 		for i := beginIndex; i <= endIndex; i++ {
-			if len(strconv.Itoa(i))%2 != 0 {
-				continue
-			}
-			middle := len(strconv.Itoa(i)) / 2
-
-			firstHalf := strconv.Itoa(i)[:middle]
-			secondHalf := strconv.Itoa(i)[middle:]
-			// fmt.Printf("First half: %s, Second half: %s", firstHalf, secondHalf)
-
-			if strings.EqualFold(firstHalf, secondHalf) {
-				sum = sum + i
+			// Check if there's any subsequence, if yes find and add all the subsequences.
+			if isInvalidID(i) {
+				sum += i
 			}
 		}
 	}
 	return sum
+}
+
+// isInvalidID creates a lsp array and returns
+// true if the id has repeated pattern.
+func isInvalidID(n int) bool {
+	num := strconv.Itoa(n)
+	l := len(num)
+	lps := make([]int, l)
+
+	i := 1
+	length := 0
+
+	for i < l {
+		if num[i] == num[length] {
+			length++
+			lps[i] = length
+			i++
+		} else {
+			if length != 0 {
+				length = lps[length-1]
+			} else {
+				lps[i] = 0
+				i++
+			}
+		}
+	}
+
+	return lps[l-1] > 0 && l%(l-lps[l-1]) == 0
 }
